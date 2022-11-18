de4d4c989f94a6784ce6592e1336a723966782a3
Jordan Cannon <LiteracyFanatic@gmail.com>
2022-11-17 20:03:32 -0600

    Return early with default value if no argument is passed

--- dpk.js
+++ dpk.js
@@ -9,23 +9,23 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = createPartitionKey(data);
-    }
-  }
-
-  if (candidate === undefined) {
+  if (event === undefined) {
     return TRIVIAL_PARTITION_KEY;
   }
 
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = createPartitionKey(data);
+  }
+
+  // Ensure we return a string
   if (typeof candidate !== "string") {
     candidate = JSON.stringify(candidate);
   }
 
+  // Truncate the result if too long
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = createPartitionKey(candidate);
   }
